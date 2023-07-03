3dbb1b6365bfc0a35d49b23b8379d44ddcf834df
Fredrick <mwangikingz94@gmail.com>
2023-07-03 06:55:59 +0300

    adding controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hero.destroy_all
+Power.destroy_all
+HeroPower.destroy_all
+
+heroes = [
+  { name: 'Kamala Khan', super_name: 'Ms. Marvel' },
+  { name: 'Doreen Green', super_name: 'Squirrel Girl' },
+  { name: 'Gwen Stacy', super_name: 'Spider-Gwen' }
+]
+
+powers = [
+  { name: 'Super Strength', description: 'Gives the wielder super-human strength' },
+  { name: 'Flight', description: 'Gives the wielder the ability to fly through the skies at supersonic speed' }
+]
+
+heroes.each do |hero_attrs|
+  Hero.create(hero_attrs)
+end
+
+powers.each do |power_attrs|
+  Power.create(power_attrs)
+end
+
+heroes = Hero.all
+powers = Power.all
+
+heroes.each do |hero|
+  powers.each do |power|
+    HeroPower.create(strength: ['Strong', 'Average', 'Weak'].sample, hero: hero, power: power)
+  end
+end
